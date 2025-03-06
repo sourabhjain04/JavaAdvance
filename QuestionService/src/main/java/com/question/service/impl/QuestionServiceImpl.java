bfe223db453b06bf54bde06d39db80da3ae3bca0
Sourabh Jain <110473469+sourabhjain04@users.noreply.github.com>
2025-03-06 19:36:34 +0530

    Question service for microservice

---
+++ QuestionService/src/main/java/com/question/service/impl/QuestionServiceImpl.java
@@ -0,0 +1,34 @@
+package com.question.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.question.entities.Question;
+import com.question.repository.QuestionRepository;
+import com.question.service.QuestionService;
+
+@Service
+public class QuestionServiceImpl implements QuestionService {
+    private QuestionRepository questionRepository;
+    public QuestionServiceImpl(QuestionRepository questionRepository) {
+        this.questionRepository = questionRepository;
+    }
+    @Override
+    public Question create(Question question) {
+        return questionRepository.save(question);
+    }
+    @Override
+    public List<Question> get() {
+        return questionRepository.findAll();
+    }
+    @Override
+    public Question getOne(Long id) {
+        return questionRepository.findById(id).orElseThrow(() -> new RuntimeException("Question not found !!"));
+    }
+
+    @Override
+    public List<Question> getQuestionsOfQuiz(Long quizId) {
+        return questionRepository.findByQuizId(quizId);
+    }
+}
--- firstjobapp/src/main/java/com/koenig/firstjobapp/job/Job.java
+++ firstjobapp/src/main/java/com/koenig/firstjobapp/job/Job.java
@@ -5,7 +5,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
 
+@Data
 @Entity
 //@Table(name="job_table")
 public class Job {
@@ -19,65 +21,5 @@ public class Job {
     private String maxSalary;
     private String location;
 
-    public Job(Long id, String title, String description, String minSalary, String maxSalary, String location) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.minSalary = minSalary;
-        this.maxSalary = maxSalary;
-        this.location = location;
-    }
-
-    public Job() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getMinSalary() {
-        return minSalary;
-    }
-
-    public void setMinSalary(String minSalary) {
-        this.minSalary = minSalary;
-    }
-
-    public String getMaxSalary() {
-        return maxSalary;
-    }
-
-    public void setMaxSalary(String maxSalary) {
-        this.maxSalary = maxSalary;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
+ 
 }
